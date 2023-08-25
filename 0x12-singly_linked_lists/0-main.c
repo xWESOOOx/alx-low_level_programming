d64048131658ac289bef09a1b100e46ad2757eca
xWESOOOx <kishkywesal@gmail>
2023-08-24 19:34:00 -0700

    0x12. C - Singly linked lists

--- 0x12-singly_linked_lists/0-main.c
+++ 0x12-singly_linked_lists/0-main.c
@@ -22,3 +22,19 @@ int main(void)
         printf("Error\n");
         return (1);
     }
+    new->str = strdup("Hello");
+    new->len = 5;
+    new->next = head;
+    head = new;
+    n = print_list(head);
+    printf("-> %lu elements\n", n);
+
+    printf("\n");
+    free(new->str);
+    new->str = NULL;
+    n = print_list(head);
+    printf("-> %lu elements\n", n);
+
+    free(new);
+    return (0);
+}
