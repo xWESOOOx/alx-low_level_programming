887c1f1ca469e05bd3b65160fa17d7af5cb65739
xWESOOOx <kishkywesal@gmail>
2023-09-04 17:03:13 -0700

    0x15. C - File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,38 +1,28 @@
-#include "holberton.h"
-/**
- * read_textfile - read a textfile.
- * @filename: name of the file to open.
- * @letters: number of letters to print in the standar ouput.
- *
- * Return: number of letter printed.
- */
+#include "main.h"
+#include <stdlib.h>
 
+/**
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd_read, count_chars, fd_open;
-	char *buf_letters;
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-	if (filename == NULL)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-	fd_open = open(filename, O_RDONLY);
-	if (fd_open == -1)
-		return (0);
-	buf_letters = malloc(sizeof(char) * letters);
-	if (buf_letters == NULL)
-		return (0);
-	fd_read = read(fd_open, buf_letters, letters);
-	if (fd_read == -1)
-	{
-		free(buf_letters);
-		return (0);
-	}
-	count_chars = write(STDOUT_FILENO, buf_letters, fd_read);
-	if (count_chars == -1)
-	{
-		free(buf_letters);
-		return (0);
-	}
-	close(fd_open);
-	free(buf_letters);
-	return (count_chars);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
+
+	free(buf);
+	close(fd);
+	return (w);
 }
